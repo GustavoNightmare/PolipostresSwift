cb5da17bf333ec0fb62ef36b70086db5d38b1434
GustavoNightmare <gsandovalm@unicauca.edu.co>
2025-11-01 08:47:13 -0500

    Initial Commit

---
+++ SwiftPolipostres/SwiftPolipostresApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPolipostresApp.swift
+//  SwiftPolipostres
+//
+//  Created by Telematica on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPolipostresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftPolipostresTests/SwiftPolipostresTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPolipostresTests.swift
+//  SwiftPolipostresTests
+//
+//  Created by Telematica on 1/11/25.
+//
+
+import Testing
+@testable import SwiftPolipostres
+
+struct SwiftPolipostresTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
